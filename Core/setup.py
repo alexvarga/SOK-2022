79e12076e418e7abdff1d4faf391868bd9c402da
Aleksandra <aleksandra.varga@yahoo.com>
2022-08-08 18:08:11 +0200

    Added dynamic plugin loading in settings.py in the main project app

--- Core/setup.py
+++ Core/setup.py
@@ -5,7 +5,13 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=['Django>=1.10'],
-
     package_data={'core': ['static/core/*.*', 'templates/core/*.html']},
+    entry_points={
+        'INSTALLED_APPS.load':
+            ['Config=core.apps.CoreConfig'],
+        'URL.load':
+            ['Url=core', 'Path=Core.urls']
+    },
+
     zip_safe=False
 )
