205a66abc894fda56d119b7714f912e86aaaccfc
Aleksandra <aleksandra.varga@yahoo.com>
2022-08-09 00:02:20 +0200

    Added index.html and index view. Managed including core\urls.py into main project urls.

--- Core/core/views.py
+++ Core/core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.apps.registry import apps
+
 
 # Create your views here.
+
+
+def index(request):
+    plugins = apps.get_app_config('core').source_plugins
+    return render(request, 'core/index.html', {'source_plugins': plugins})
