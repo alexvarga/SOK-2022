c7034b1fe844c252ad9ebe419b59a853ec294bda
Aleksandra <aleksandra.varga@yahoo.com>
2022-08-09 20:45:58 +0200

    Implemented the most basic of html parsers :) just prints text.

--- SourceHtml/source_html/services/source_from_html.py
+++ SourceHtml/source_html/services/source_from_html.py
@@ -1,3 +1,32 @@
+from lxml import etree
+
+
 
 class LoadHtmlSource:
-    pass
+
+    def parseHTML(self):
+        print("html parser bb")
+        html = "<html><body><div><h1>Prvi naslov</h1></div><h1>Heading 1</h1><h2>Heading 2</h2><h3>Heading 3</h3><h4>Heading 4</h4><h5>Heading 5</h5><h6>Heading 6</h6></body></html>"
+        root = etree.fromstring(html)
+
+        nodes = root
+
+        test=[]
+
+
+        while True:
+            newNodes=[]
+            if len(nodes) == 0:
+                print('stigli smo do 0')
+                break
+            for node in nodes:
+                print(node)
+                test.append(node)
+
+                if len(node)>0:
+                    for child in node:
+                        newNodes.append(child)
+            nodes=newNodes
+
+        for item in test:
+            print(item.text, "item")
--- SourceHtml/source_html/views.py
+++ SourceHtml/source_html/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.apps.registry import apps
-
+from .services.source_from_html import LoadHtmlSource
 
 # Create your views here.
 
 
 def index(request):
 
+    a = LoadHtmlSource()
+    a.parseHTML()
     return redirect('../../core')
