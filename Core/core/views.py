1eb32c636de46b2592fadddbed725b343258fd11
Aleksandra <aleksandra.varga@yahoo.com>
2022-08-10 22:12:28 +0200

    Implemented file uploading.

--- Core/core/views.py
+++ Core/core/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.apps.registry import apps
+from .services.UploadHandlerService import handle_uploaded_file
+from .forms import UploadForm
 
 
 # Create your views here.
@@ -7,4 +10,19 @@ from django.apps.registry import apps
 
 def index(request):
     plugins = apps.get_app_config('core').source_plugins
-    return render(request, 'core/index.html', {'source_plugins': plugins})
+    form = UploadForm()
+    return render(request, 'core/index.html', {'source_plugins': plugins, 'form': form})
+
+
+def upload(request):
+    plugins = apps.get_app_config('core').source_plugins
+
+    form = UploadForm()
+    if request.method == "POST":
+        form = UploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponse("FILE UPLOADED SUCCESSFULLY")
+        else:
+            form = UploadForm()
+    return render(request, 'blog/home.html', {'form': form})
